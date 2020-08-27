5f84e83a6356e26556391925d462c2919dae6b6d
MantaranSingh <meekmantaran@gmail.com>
2020-08-27 13:53:41 +0530

    added leaderboard html

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,11 +1,11 @@
-let input=document.getElementById('hostInput')
-input.addEventListener('input',()=>{
-    
-    let categories=document.getElementsByTagName('li')
-    Array.from(categories).forEach(elements=>{
-        
-        let categoryText=elements.innerText.toLowerCase()
-        if(categoryText.includes(input.value)){
+let input = document.getElementById('hostInput')
+let categories = document.getElementsByTagName('li')
+input.addEventListener('input', () => {
+
+    Array.from(categories).forEach(elements => {
+
+        let categoryText = elements.innerText.toLowerCase()
+        if (categoryText.includes(input.value)) {
             elements.style.display = "block";
         }
         else {
